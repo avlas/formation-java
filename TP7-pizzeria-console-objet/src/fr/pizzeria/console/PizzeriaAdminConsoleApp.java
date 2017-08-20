c2af90b1a2d54e0b19c088779148141b702835af
Avl <alina.vlas@gmail.com>
2017-08-20 23:06:57 +0200

    refactor

--- TP7-pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
+++ TP7-pizzeria-console-objet/src/fr/pizzeria/console/PizzeriaAdminConsoleApp.java
@@ -21,7 +21,7 @@ public class PizzeriaAdminConsoleApp {
 
 	// Initialise the scanner who will ask questions to the user
 	public static Scanner input = new Scanner(System.in);
-	
+
 	// Initialise the validator who will check the user entries
 	public static UserEntriesValidator validator = new UserEntriesValidator();
 
@@ -33,31 +33,32 @@ public class PizzeriaAdminConsoleApp {
 
 		do {
 			showMenu();
+			
 			try {
 				userOption = input.nextInt();
 
 				switch (userOption) {
-					case 1:
-						listPizzas = new ListAllPizzasOptionMenu(dao);
-						listPizzas.execute();
-						break;
-	
-					case 2:
-						AddPizzaOptionMenu addPizza = new AddPizzaOptionMenu(dao);
-						addPizza.execute();
-						break;
-	
-					case 3:
-						listPizzas = new ListAllPizzasOptionMenu(dao);
-						listPizzas.execute();
-	
-						UpdatePizzaOptionMenu updatePizza = new UpdatePizzaOptionMenu(dao);
-						updatePizza.execute();
-						break;
-	
-					case 4:
-						listPizzas = new ListAllPizzasOptionMenu(dao);
-						listPizzas.execute();
+				case 1:
+					listPizzas = new ListAllPizzasOptionMenu(dao);
+					listPizzas.execute();
+					break;
+
+				case 2:
+					AddPizzaOptionMenu addPizza = new AddPizzaOptionMenu(dao);
+					addPizza.execute();
+					break;
+
+				case 3:
+					listPizzas = new ListAllPizzasOptionMenu(dao);
+					listPizzas.execute();
+
+					UpdatePizzaOptionMenu updatePizza = new UpdatePizzaOptionMenu(dao);
+					updatePizza.execute();
+					break;
+
+				case 4:
+					listPizzas = new ListAllPizzasOptionMenu(dao);
+					listPizzas.execute();
 
 					DeletePizzaOptionMenu deletePizza = new DeletePizzaOptionMenu(dao);
 					deletePizza.execute();
@@ -85,7 +86,7 @@ public class PizzeriaAdminConsoleApp {
 		System.out.println("\n****************************************** ");
 		System.out.println("*****   Pizzeria Administration  ********* ");
 		System.out.println("****************************************** ");
-		
+
 		System.out.println("1. List all pizzas");
 		System.out.println("2. Add new pizza");
 		System.out.println("3. Modify a pizza");
